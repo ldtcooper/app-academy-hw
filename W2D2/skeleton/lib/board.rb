9194a9817569b529ef55a5b908200fdb7daa7ab4
Logan Cooper <ldtcoop@gmail.com>
2017-08-07 22:00:20 -0700

    Add partial make_move and next_turn classes

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -2,16 +2,41 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = place_stones
+    @name1 = name1
+    @name2 = name2
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    Array.new(14).map.with_index do |_cup, ind|
+      if ind == 6 || ind == 13
+        []
+      else
+        [:stone, :stone, :stone, :stone]
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    unless start_pos.between?(1, 14)
+      raise CupError.new("Invalid starting cup")
+    end
   end
 
   def make_move(start_pos, current_player_name)
+    stones_to_distribute = @cups[start_pos]
+    @cups[start_pos] = []
+    current_cup = start_pos
+    until stones_to_distribute.empty?
+      if current_cup < 14
+        current_cup += 1
+      else
+        current_cup = 0
+      end
+      @cups[current_cup] << stones_to_distribute.pop
+    end
+    render
+    next_turn(current_cup)
   end
 
   def next_turn(ending_cup_idx)
@@ -32,3 +57,5 @@ class Board
   def winner
   end
 end
+
+class CupError < StandardError; end
