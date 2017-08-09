1cceb384c2f0dd8e08d7a6f5970d22cbb2780af6
Logan Cooper <ldtcoop@gmail.com>
2017-08-08 22:01:26 -0700

    Finish tests for Dessert#initialize

--- W2D3/spec/dessert_spec.rb
+++ W2D3/spec/dessert_spec.rb
@@ -14,11 +14,17 @@ describe Dessert do
       expect(dessert.type).to eq("Brownie")
     end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(10)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq([])
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("Brownie", "ten", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
