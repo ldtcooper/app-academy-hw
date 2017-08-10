6bdb174b6cae7c85e52732834ef53ceebdd17e8b
Logan Cooper <ldtcoop@gmail.com>
2017-08-09 21:16:07 -0700

    Add octopus dance methods

--- W2D4/octopus.rb
+++ W2D4/octopus.rb
@@ -43,6 +43,18 @@ def clever(arr)
   end
 end
 
-p sluggish(fish)
-p dominant(fish).last
-p clever(fish)
+tiles_array = ["up", "right-up", "right", "right-down",
+  "down", "left-down", "left",  "left-up" ]
+
+def slow_dance(dir, arr)
+  arr.each_with_index do |el, ind|
+    return ind if el == dir
+  end
+end
+
+tiles_hash = {"up" => 0, "right-up" => 1, "right" => 2, "right-down" => 3,
+  "down" => 4, "left-down" => 5, "left" => 6,  "left-up" => 7 }
+
+def fast_dance(dir, hash)
+  hash[dir]
+end
