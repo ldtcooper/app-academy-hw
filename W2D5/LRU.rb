7eae214b9b794c0ac60d7d684464c7588212620d
Logan Cooper <ldtcoop@gmail.com>
2017-08-10 22:28:57 -0700

    Add skeleton for updating cache

--- W2D5/LRU.rb
+++ W2D5/LRU.rb
@@ -9,7 +9,6 @@ class LRUCache
   end
 
   def add(el)
-    # adds element to cache according to LRU principle
     remove_oldest if full?
     add_new(el)
   end
@@ -31,6 +30,14 @@ class LRUCache
   def full?
     @cache.length == @length
   end
+
+  def move_to_first(el)
+
+  end
+
+  def repeat?
+
+  end 
 end
 
 # class LinkedList
@@ -42,3 +49,22 @@ end
 #
 #   attr_accessor :next_el, :prev_el, :obj
 # end
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+
+p johnny_cache.count == 2
+
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+
+p johnny_cache.show #== [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
