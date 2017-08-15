bdd88a6de4f7ccf567993b0a7d280f167ac24396
Logan Cooper <ldtcoop@gmail.com>
2017-08-14 21:31:13 -0700

    Add Playwright class

--- W3D2/plays.rb
+++ W3D2/plays.rb
@@ -61,32 +61,74 @@ class Play
   end
 
   def self.find_by_playwright(name)
-
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        title, year
+      FROM
+        plays
+      JOIN playwrights ON
+        plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name = ?
+    SQL
   end
 end
 
 class Playwright
-  def initialize
-
+  def initialize(options)
+    @name = options["name"]
+    @birth_year = options["birth_year"]
   end
 
   def self.all
-
+    data = PlayDBConnection.instance.execute("SElECT * FROM playwrights")
+    data.map { |el| Playwright.new(el) }
   end
 
   def self.find_by_name(name)
-
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SElECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
   end
 
   def create
-
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        plays (birth_year, name)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
   end
 
   def update
-
+    raise "#{self} not yet in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
   end
 
-  def get_plays
-
+  def get_plays(name)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        title, year
+      FROM
+        plays
+      JOIN playwrights ON
+        plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name = ?
+    SQL
   end
 end
