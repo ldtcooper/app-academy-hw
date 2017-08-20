fdaac463d7c9346248cd86e6872659307e3269f8
Logan Cooper <ldtcoop@gmail.com>
2017-08-19 18:40:54 -0700

    Set up databases

--- W4D1/app/models/book.rb
+++ W4D1/app/models/book.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  author     :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Book < ApplicationRecord
 end
