dc055ff1e40afa8e238c45ead068209a04b4af85
Logan Cooper <ldtcoop@gmail.com>
2017-08-23 22:04:02 -0700

    Finish W4D4 HW

--- W4D4/auth/app/controllers/users_controller.rb
+++ W4D4/auth/app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require[:user].permit(:username, :password)
   end
 end
--- W4D4/auth/app/models/user.rb
+++ W4D4/auth/app/models/user.rb
@@ -11,14 +11,43 @@
 #
 
 class User < ApplicationRecord
+  before_validation :ensure_session_token
   validates :username, uniqueness: true, presence: true
   validates :password_digest, uniqueness: {message: 'Password cannot be blank!'}
+  validates :password, length: {minimum: 6, allow_nil: true}
+  validates :session_token, presence: true
+
+  def password
+    @password
+  end
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    # creates a random seesion token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
   end
 end
