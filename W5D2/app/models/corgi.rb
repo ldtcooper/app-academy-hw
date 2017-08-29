e43349f80c8051652d80081c26593fd78651b280
Logan Cooper <ldtcoop@gmail.com>
2017-08-28 20:52:17 -0700

    Migrate Toys table

--- W5D2/app/models/corgi.rb
+++ W5D2/app/models/corgi.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: corgis
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Corgi < ApplicationRecord
 end
