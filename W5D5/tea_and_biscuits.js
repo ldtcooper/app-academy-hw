668253686b6c5d87f7d10abcb66d0199db95a534
Logan Cooper <ldtcoop@gmail.com>
2017-08-31 21:19:01 -0700

    Draft for tea and buscuits

--- W5D5/tea_and_biscuits.js
+++ W5D5/tea_and_biscuits.js
@@ -6,12 +6,24 @@ const reader = readline.createInterface({
 });
 
 function teaAndBiscuits(){
-  let first, second;
+  let first;
+  let second = 'no';
 
-  reader.question("Would you like some tea?\n", (res) =>{
-    first = res;
-    console.log(`You replied ${res}`);
+  reader.question("Would you like some tea?\n", (res1) =>{
+    first = res1;
+    console.log(`You replied ${res1}`);
+    if (first === "yes") {
+      reader.question("Would you like some biscuits?\n", (res2) =>{
+        second = res2;
+        console.log(`You replied ${res2}`);
+      });
+    }
   });
+
+  const firstResponse = (first === 'yes') ? 'do' : "don't";
+  const secondResponse = (second === 'yes') ? 'do' : "don't";
+  console.log(`So you ${firstResponse} want tea and ${secondResponse} want buscuits?`)
 }
+
 
 teaAndBiscuits();
