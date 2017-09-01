2241d8c1978bc74ccaa003d0423ef565f9fce13b
Logan Cooper <ldtcoop@gmail.com>
2017-08-31 20:54:17 -0700

    Finish tea question

--- W5D5/tea_and_biscuits.js
+++ W5D5/tea_and_biscuits.js
@@ -4,3 +4,14 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+function teaAndBiscuits(){
+  let first, second;
+
+  reader.question("Would you like some tea?\n", (res) =>{
+    first = res;
+    console.log(`You replied ${res}`);
+  });
+}
+
+teaAndBiscuits();
