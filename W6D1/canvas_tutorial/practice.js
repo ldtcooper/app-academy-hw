393855865a246cc40387652cb689fed7e616a4fb
Logan Cooper <ldtcoop@gmail.com>
2017-09-04 20:48:41 -0700

    W6D2 initial commit

--- W6D1/canvas_tutorial/practice.js
+++ W6D1/canvas_tutorial/practice.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
+  let canvas = document.getElementById('myCanvas');
+  canvas.height = 500;
+  canvas.width = 500;
+  let ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'red';
+  ctx.fillRect(10, 50, 100, 100);
 
+  ctx.beginPath();
+  ctx.arc(100, 100, 20, 0, 360);
+  ctx.strokeStyle = "green";
+  ctx.lineWidth = 2;
+  ctx.fillStyle = "pink";
+  ctx.fill();
+  ctx.stroke();
 });
