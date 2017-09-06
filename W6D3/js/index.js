a190c3cd26f9d6c00e4e28ee79add1f6b2f64326
Logan Cooper <ldtcoop@gmail.com>
2017-09-05 21:12:18 -0700

    Add AJAX request skeleton

--- W6D3/js/index.js
+++ W6D3/js/index.js
@@ -1,5 +1,14 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+  type: ,
+  url: ,
+  success(data) {
 
+  },
+  error() {
+
+  }
+});
 // Add another console log here, outside your AJAX request
