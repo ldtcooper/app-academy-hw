5bc657a95257c6792226d1a1d30ee42a463b52a0
Logan Cooper <ldtcoop@gmail.com>
2017-09-10 14:45:16 -0700

    Add default switch statement to reducer

--- W7D1/frontend/reducer.js
+++ W7D1/frontend/reducer.js
@@ -4,7 +4,9 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch(action.type) {
+    default: return state;
+  }
 };
 
 export default reducer;
