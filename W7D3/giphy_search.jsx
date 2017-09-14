83bf055877ea8afbd376d4682370b9404d3c3db2
Logan Cooper <ldtcoop@gmail.com>
2017-09-13 21:33:37 -0700

    Finish redux cycle

--- W7D3/giphy_search.jsx
+++ W7D3/giphy_search.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {configureStore} from './store/store';
 import Root from './components/root';
-import { fetchSearchGiphys } from './util/api_util';
-import { receiveSearchGiphys } from './actions/giphy_actions';
+import { receiveSearchGiphys, fetchSearchGiphys } from './actions/giphy_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
--- W7D3/store/store.js
+++ W7D3/store/store.js
@@ -3,5 +3,5 @@ import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
 export const configureStore = () => {
-  return createStore(RootReducer);
+  return createStore(RootReducer, applyMiddleware(thunk));
 };
