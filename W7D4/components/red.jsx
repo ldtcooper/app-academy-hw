ae97c29631a6931320be663ff4e7f83005400039
Logan Cooper <ldtcoop@gmail.com>
2017-09-13 20:50:43 -0700

    Finish color routes

--- W7D4/components/red.jsx
+++ W7D4/components/red.jsx
@@ -10,10 +10,11 @@ class Red extends React.Component {
         <h2 className="red"></h2>
         {/* Links here */}
 
-        {/* Routes here */}
+        <Route path="/red/yellow" component={Yellow}/>
+        <Route path="/red/orange" component={Orange}/>
       </div>
     );
   }
-};
+}
 
 export default Red;
