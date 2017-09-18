f9762eb52ac6acaaa5920b8f712fc30c2287f637
Logan Cooper <ldtcoop@gmail.com>
2017-09-17 20:09:27 -0700

    Initial W8D1 commit

--- W7D3/components/giphys_search.jsx
+++ W7D3/components/giphys_search.jsx
@@ -1,3 +1,41 @@
 import React from 'react';
-
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Container {
+  constructor(props) {
+    super(props);
+    this.state = {searchTerm: ""};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({searchTerm: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let searchTerm = this.state.searchTerm.split(" ").join("+");
+    this.props.fetchSearchGiphys(searchTerm);
+  }
+
+  render() {
+      return (
+        <div>
+          <form>
+            <label>
+              Search:
+              <input
+                type="text"
+                name="search"
+                value={this.state.searchTerm}
+                onChange={this.handleChange}/>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      );
+  }
+}
+
+export default GiphysSearch;
